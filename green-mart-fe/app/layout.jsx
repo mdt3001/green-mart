6646eb8c632816e89e754d71e820e160e05f06ea
mdt3001 <22521413@gm.uit.edu.vn>
2025-10-11 00:12:10 +0700

    delete(inngest cloud)

--- green-mart-fe/app/layout.jsx
+++ green-mart-fe/app/layout.jsx
@@ -1,27 +1,27 @@
 import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
-import StoreProvider from "@/app/StoreProvider";
+import StoreProvider from "./StoreProvider";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-    title: "Green Mart. - Shop smarter",
-    description: "Green Mart. - Shop smarter",
+  title: "Green Mart. - Shop smarter",
+  description: "Green Mart. - Shop smarter",
 };
 
 export default function RootLayout({ children }) {
-    return (
-      <ClerkProvider>
-        <html lang="en">
-          <body className={`${outfit.className} antialiased`}>
-            <StoreProvider>
-              <Toaster />
-              {children}
-            </StoreProvider>
-          </body>
-        </html>
-      </ClerkProvider>
-    );
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${outfit.className} antialiased`}>
+          <StoreProvider>
+            <Toaster />
+            {children}
+          </StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
