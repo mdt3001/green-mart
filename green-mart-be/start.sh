8bc6be57a67ea42123b0a6ed58a472bab9536174
Croslee <Celros2004@gmail.com>
2025-12-07 18:26:16 +0700

    Fix: Add chatbot seeder to production deployment

--- green-mart-be/start.sh
+++ green-mart-be/start.sh
@@ -8,6 +8,9 @@ php artisan view:cache
 # Run migrations
 php artisan migrate --force
 
+# Seed chatbot intents
+php artisan db:seed --class=ChatIntentSeeder --force
+
 # Start nginx
 service nginx start
 
